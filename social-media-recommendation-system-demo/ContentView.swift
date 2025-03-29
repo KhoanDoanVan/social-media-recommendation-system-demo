f8fb223743ce669c594450a22f2b9ba6478c200e
khoanisdev <doanvankhoan124@gmail.com>
2025-03-29 15:12:41 +0700

    Initial Commit

---
+++ social-media-recommendation-system-demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  social-media-recommendation-system-demo
+//
+//  Created by Đoàn Văn Khoan on 29/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ social-media-recommendation-system-demo/social_media_recommendation_system_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  social_media_recommendation_system_demoApp.swift
+//  social-media-recommendation-system-demo
+//
+//  Created by Đoàn Văn Khoan on 29/3/25.
+//
+
+import SwiftUI
+
+@main
+struct social_media_recommendation_system_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
